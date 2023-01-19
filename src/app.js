10056bfd86bf92459f78cbf70c5160d81c2f3fc1
adarshchhetri9 <chhetriadarsh912@gmail.com>
2023-01-19 14:39:16 +0530

    auto user name and pic

--- src/app.js
+++ src/app.js
@@ -1,20 +1,32 @@
 import Navbar from "./components/Navbar";
 import Card from "./components/Card";
 import Counter from "./components/Counter";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [details, setDetails] = useState({});
+
+  const fetchDetails = async () => {
+    const { data } = await axios.get("https://randomuser.me/api/");
+    const detail = data.results[0];
+    console.log(detail)
+    setDetails(detail);
+  };
+
+  useEffect(() => {
+    fetchDetails();
+  }, []);
+
   return (
     <>
-      <Counter/>
+      <Counter />
       <Navbar />
       <h1 className="text-3xl font-bold bg-gray-700 underline">
         Adarsh chhetri
       </h1>
       <p className="text-2xl">this is me coding</p>
-      <Card
-        img="https://as01.epimg.net/meristation_en/imagenes/2022/07/23/news/1658585691_407651_1658585843_noticia_normal.jpg"
-        ler="happy me"
-      />
+      <Card details={details} />
     </>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,4 @@
-function Card(props) {
+function Card({ details }) {
   return (
     <section className="text-gray-600 body-font">
       <div className="container px-5 py-24 mx-auto">
@@ -7,15 +7,15 @@ function Card(props) {
             <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
               <img
                 className="lg:h-48 md:h-36 w-full object-cover object-center"
-                src={props.img}
+                src={details.picture?.large}
                 alt="blog"
               />
               <div className="p-6">
                 <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
-                  CATEGORY
+                {details.name?.title}
                 </h2>
                 <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                  The Catalyzer
+                  {details.name?.first}
                 </h1>
                 <p className="leading-relaxed mb-3">
                   Photo booth fam kinfolk cold-pressed sriracha leggings
@@ -23,7 +23,7 @@ function Card(props) {
                 </p>
                 <div className="flex items-center flex-wrap ">
                   <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
-                    {props.ler}
+                    {details.name?.first}
                     <svg
                       className="w-4 h-4 ml-2"
                       viewBox="0 0 24 24"
